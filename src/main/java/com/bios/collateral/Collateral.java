2d0f56b2d6aaa7ae111a49d7882d41fcbcf1f328
barnabeepickle <linusrk30@gmail.com>
2025-09-17 10:40:30 -0400

    fixed logger setup

--- src/main/java/com/bios/collateral/Collateral.java
+++ src/main/java/com/bios/collateral/Collateral.java
@@ -3,16 +3,16 @@ package com.bios.collateral;
 import net.fabricmc.api.ModInitializer;
 
 import net.minecraft.util.Identifier;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.util.HashMap;
 import java.util.UUID;
 
 public class Collateral implements ModInitializer {
 	public static final String MOD_ID = "collateral";
-
-	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+	// setup the logger
+	public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
 
 	public static final HashMap<UUID, Boolean> VEINMINE_PRESSED = new HashMap<>();
 
